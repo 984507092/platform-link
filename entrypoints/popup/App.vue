7bf80a103784f75936dd212ffedc5ab12ea72b0b
SmallTeddy <zhangzeming1212@gmail.com>
2024-03-27 20:09:59 +0800

    feat: add unocss config and add input

--- entrypoints/popup/App.vue
+++ entrypoints/popup/App.vue
@@ -1,15 +1,25 @@
 <template>
-  <div class="wrapper">
-    <catalogue></catalogue>
+  <div w-full h-full flex-col>
+    <div class="catalogue-link" pb-32px>
+      <catalogue></catalogue>
+    </div>
+    <div h-40px fixed bottom-0 w-320px style="background: #333">
+      <input w-full h-full class="search-input" type="text" v-model="search" />
+    </div>
   </div>
 </template>
 <script lang="ts" setup>
 import catalogue from "../view/catalogue.vue"
 
+const search = ref('')
 </script>
+
 <style scoped>
-.wrapper {
-  width: 100%;
-  height: 100%;
+.search-input:focus-visible {
+  font-size: 18px;
+  box-sizing: border-box;
+  border: 1px solid #ccc;
+  border-radius: 0!important;
+  outline: none;
 }
 </style>
